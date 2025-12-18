aea1e37002058a6dd241d04ec91ffa15924201b4
AdrianeSS <adriane_silvasanches@hotmail.com>
2025-12-17 23:49:55 -0300

    feat: iniciando base do projeto

---
+++ dashboard-host/src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold text-gray-800">
+        Meu Projeto
+      </h1>
+      {/* Seus componentes virão aqui */}
+    </div>
+  )
+}
